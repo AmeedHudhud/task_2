d7876f7068104daec62c310e63d3b526937595ee
Ameed Hudhud <Ameed.Hudhud@exalt.ps>
2024-03-04 16:24:45 +0200

    add sign in

--- cypress/JPetStore.cy.js
+++ cypress/JPetStore.cy.js
@@ -1,35 +1,35 @@
-import * as Storehelper from "./support/JPetStoreHelper"
+import * as storeHelper from "./support/JPetStoreHelper"
 
 /**
  * test case to check this page "https://petstore.octoperf.com/actions/Catalog.action" 
  */
 
 const VALID_CREDENTIALS = {
-    'name': 'ameed',
-    'password': 'ameed0595'
+    name: 'ameed',
+    password: 'ameed0595'
 }
 const INVALID_CREDENTIALS = {
-    'name': 'qqq',
-    'password': '12345'
+    name: 'qqq',
+    password: '12345'
 }
 describe('JPetStore', () => {
     beforeEach(() => {
         cy.visit('https://petstore.octoperf.com/actions/Catalog.action')
     })
     it('sign in by valid user name and valid password ', () => {
-        Storehelper.signin(VALID_CREDENTIALS.name,VALID_CREDENTIALS.password)
+        storeHelper.signin(VALID_CREDENTIALS.name, VALID_CREDENTIALS.password)
     })
     it('sign in by invalid user name and valid password', () => {
-        Storehelper.signin(VALID_CREDENTIALS.name, VALID_CREDENTIALS.password, false)
+        storeHelper.signin(INVALID_CREDENTIALS.name, VALID_CREDENTIALS.password, false)
     })
     it('sign in by invalid passworrd and valid user name', () => {
-        Storehelper.signin(VALID_CREDENTIALS.name, INVALID_CREDENTIALS.password, false)
+        storeHelper.signin(VALID_CREDENTIALS.name, INVALID_CREDENTIALS.password, false)
     })
     it('sign in by only fill username', () => {
-        Storehelper.signin(VALID_CREDENTIALS.name, '', false)
+        storeHelper.signin(VALID_CREDENTIALS.name, '', false)
     })
     it('sign in by only fill password', () => {
-        Storehelper.signin(' ', VALID_CREDENTIALS.password, false)
+        storeHelper.signin('', VALID_CREDENTIALS.password, false)
     })
 })
 
--- cypress/support/JPetStoreHelper.js
+++ cypress/support/JPetStoreHelper.js
@@ -1,59 +1,37 @@
-export const insertData = (username, password, empty = true) => {
-    cy.contains('Sign In')
-        .click()
-    if (empty == false) {
-        cy.get(LOCATORS.username_field)
-            .clear()
-            .type(username)
-        cy.get(LOCATORS.password_field)
-            .clear()
-            .type(password)
-    } else {
-        if (username == ' ') {
-            cy.get(LOCATORS.username_field)
-                .clear()
-            cy.get(LOCATORS.password_field)
-                .clear()
-                .type(password)
-        } else {
-            cy.contains('Sign In')
-                .click()
-            cy.get(LOCATORS.username_field)
-                .clear()
-                .type(username)
-            cy.get(LOCATORS.password_field).invoke('val', '')
-        }
-    }
-    cy.get(LOCATORS.signin_button)
-        .click()
+export const LOCATORS = {
+    username_field: '[name="username"]',
+    password_field: '[name="password"]',
+    signin_button: '[name="signon"]',
+    error_messages : '.messages li',
+    welcomeMessage : '[id="WelcomeContent"]'
 }
-export const signin = (username, password, expected = true) => {
-    if (expected == true) {
-        insertData('ameed', 'ameed0595', false)
-        cy.get('[id="WelcomeContent"]').invoke('text').should('contain', 'Welcome ameed!')
+export const ERROR_MESSAGE = {
+    message1: 'Invalid username or password.  Signon failed.'
+}
+export const CONSTANT_WORD = {
+    signin : 'Sign In',
+    login : 'Login'
+}
+export const enterFieldValue = (value, locator) => {
+    cy.get(locator).clear().invoke('val', value)
+}
+export const clickButton = (locator) => {
+    cy.contains(locator).click()
+}
+export const signin = (username, password, validCred = true) => {
+    clickButton(CONSTANT_WORD.signin)
+    enterFieldValue(username, LOCATORS.username_field)
+    enterFieldValue(password, LOCATORS.password_field)
+    clickButton(CONSTANT_WORD.login)
+    if (validCred == true) {
+        cy.get(LOCATORS.welcomeMessage).invoke('text').should('contain', `Welcome ${username}!`)
     } else {
-        if (username == ' ') {
-            insertData(username, password, true)
+        if (username == '') {
             cy.get(LOCATORS.username_field).should('have.class', 'error')
         } else if (password == '') {
-            insertData(username, password, true)
             cy.get(LOCATORS.password_field).should('have.class', 'error')
-        } else if (username != 'ameed') {
-            insertData(username, password, false)
-            cy.get(LOCATORS.error_message)
-                .invoke('text')
-                .should('contain', 'Invalid username or password.  Signon failed.')
-        } else if (password != 'ameed0595') {
-            insertData(username, password, false)
-            cy.get(LOCATORS.error_message)
-                .invoke('text')
-                .should('contain', 'Invalid username or password.  Signon failed.')
+        } else {
+            cy.get(LOCATORS.error_messages).should('contain', ERROR_MESSAGE.message1)
         }
     }
-}
-export const LOCATORS = {
-    username_field : '[name="username"]',
-    password_field : '[name="password"]',
-    signin_button : '[name="signon"]',
-    error_message : '.messages',
 }
