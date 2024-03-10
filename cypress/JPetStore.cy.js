3bad6b85afcab452e907dd99f2ccad53515d48e6
Ameed Hudhud <Ameed.Hudhud@exalt.ps>
2024-03-10 14:50:07 +0200

    edit

--- cypress/JPetStore.cy.js
+++ cypress/JPetStore.cy.js
@@ -10,85 +10,19 @@ const INVALID_CREDENTIALS = {
     NAME: "qqq",
     PASSWORD: "12345",
 };
-const FISH = {
-    NAME: 'FISH',
-    ANGEL_FISH: "FI-SW-01",
-    LARGE_ANGEL_FISH: "EST-1",
-    SMALL_ANGEL_FISH: "EST-2",
-
-    TYPE: 'Angelfish',
-    DESCRIPTION: 'Large Angelfish',
-    PRICE: '$16.50'
-};
-const DOG = {
-    NAME: 'DOG',
-    BULL_DOG: "K9-BD-01",
-    MALE_ADULT_BULL_DOg: "EST-6",
-    FEMALE_PUPPY_BULL_DOG: "EST-7",
-};
-const CAT = {
-    NAME: 'CAT',
-    MANX: "FL-DSH-01",
-    TAILLESS_MAX: "EST-14",
-};
-const REPTILES = {
-    NAME: 'REPTILES',
-    IGUANA: "RP-LI-02",
-    GREEN_ADULT_IGUANA: "EST-13",
-};
-const BIRD = {
-    NAME: 'BIRD',
-    FINCH: "AV-SB-02",
-    ADULT_MALE_FINCH: "EST-19",
-};
-
-// it.only('test', () => {
-//     cy.visit("https://petstore.octoperf.com/actions/Catalog.action");
-//     // storeHelper.addProducts(
-//     //     [{
-//     //             type: storeHelper.LOCATORS.fish,
-//     //             name: FISH.ANGEL_FISH,
-//     //             Description: FISH.LARGE_ANGEL_FISH,
-//     //         },
-//     //         {
-//     //             type: storeHelper.LOCATORS.fish,
-//     //             name: FISH.ANGEL_FISH,
-//     //             Description: FISH.LARGE_ANGEL_FISH,
-//     //         },
-//     //     ],
-//     //     false
-//     // );
-//     // storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
-//     //     //   storeHelper.givePrice(1)
-//     //     // storeHelper.giveQuantity(FISH.LARGE_ANGEL_FISH)
-//     //     // storeHelper.checkTotalCost(FISH.LARGE_ANGEL_FISH, 1)
-//     // cy.contains('Sub Total:').invoke('text').then((text) => {
-//     //     const subtotal = text.match(/\$([\d.]+)/)[1];
-//     //     cy.log(subtotal);
-//     // });
-
-
-// })
-
+const value = '10'
 describe("JPetStore", () => {
     beforeEach(() => {
         cy.visit("https://petstore.octoperf.com/actions/Catalog.action");
     });
     it("sign in by invalid user name and valid password", () => {
         storeHelper.clickButton(storeHelper.WORD_REGISTRY.SIGNIN);
-        storeHelper.signin(
-            INVALID_CREDENTIALS.NAME,
-            VALID_CREDENTIALS.PASSWORD,
-            false
-        );
+        storeHelper.signin(INVALID_CREDENTIALS.NAME,VALID_CREDENTIALS.PASSWORD,false);
     });
     it("sign in by invalid passworrd and valid user name", () => {
         storeHelper.clickButton(storeHelper.WORD_REGISTRY.SIGNIN);
-        storeHelper.signin(
-            VALID_CREDENTIALS.NAME,
-            INVALID_CREDENTIALS.PASSWORD,
-            false
-        );
+        storeHelper.signin(VALID_CREDENTIALS.NAME,INVALID_CREDENTIALS.PASSWORD,false);
+        cy.get('[name="username"]')
     });
     it("sign in by only fill username", () => {
         storeHelper.clickButton(storeHelper.WORD_REGISTRY.SIGNIN);
@@ -102,182 +36,142 @@ describe("JPetStore", () => {
         storeHelper.clickButton(storeHelper.WORD_REGISTRY.SIGNIN);
         storeHelper.signin(VALID_CREDENTIALS.NAME, VALID_CREDENTIALS.PASSWORD);
     });
-    it("add product from menu to cart and make sure it is added ", () => {
+    it("add product from information page for product to cart and verify it is added", () => {
         storeHelper.addProducts([{
-                type: FISH.NAME,
-                name: FISH.ANGEL_FISH,
-                Description: FISH.LARGE_ANGEL_FISH,
-            },
-            {
-                type: CAT.NAME,
-                name: CAT.MANX,
-                Description: CAT.TAILLESS_MAX,
-            },
-            {
-                type: DOG.NAME,
-                name: DOG.BULL_DOG,
-                Description: DOG.FEMALE_PUPPY_BULL_DOG,
-            },
+            type: storeHelper.PRODUCTS.FISH.NAME,
+            name: storeHelper.PRODUCTS.FISH.ANGEL_FISH_ID,
+            Description: storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,
+        }
+        ], false);
+        storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
+        storeHelper.verifyProductExistence([{val:storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,isExist:true}])
+        storeHelper.clickButton(storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,true)
+        storeHelper.verifyInformation(storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID, storeHelper.PRODUCTS.FISH.DESCRIPTION, storeHelper.PRODUCTS.FISH.TYPE, storeHelper.PRODUCTS.FISH.PRICE)
+    });
+    it("add product from menu to cart and verify it is added ", () => {
+        storeHelper.addProducts([{
+            type: storeHelper.PRODUCTS.FISH.NAME,
+            name: storeHelper.PRODUCTS.FISH.ANGEL_FISH_ID,
+            Description: storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,
+        },
         ]);
         storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
-        storeHelper.verifyProductExistence([FISH.LARGE_ANGEL_FISH, CAT.TAILLESS_MAX, DOG.FEMALE_PUPPY_BULL_DOG])
-
+        storeHelper.verifyProductExistence([{val:storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,isExist:true}])
+        storeHelper.clickButton(storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,true)
+        storeHelper.verifyInformation(storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID, storeHelper.PRODUCTS.FISH.DESCRIPTION, storeHelper.PRODUCTS.FISH.TYPE, storeHelper.PRODUCTS.FISH.PRICE)
     });
-    it("add product from information page for product to cart and make sure it is added", () => {
-        storeHelper.addProducts([{
-                type: FISH.NAME,
-                name: FISH.ANGEL_FISH,
-                Description: FISH.LARGE_ANGEL_FISH,
+    it("remove product from cart and verify it is removed", () => {// bug in (EST-1)
+        storeHelper.addProducts([
+            {
+                type: storeHelper.PRODUCTS.FISH.NAME,
+                name: storeHelper.PRODUCTS.FISH.ANGEL_FISH_ID,
+                Description: storeHelper.PRODUCTS.FISH.SMALL_ANGEL_FISH_ID,
             },
             {
-                type: CAT.NAME,
-                name: CAT.MANX,
-                Description: CAT.TAILLESS_MAX,
+                type: storeHelper.PRODUCTS.CAT.NAME,
+                name: storeHelper.PRODUCTS.CAT.MANX_ID,
+                Description: storeHelper.PRODUCTS.CAT.TAILLESS_MAX_ID,
             },
             {
-                type: DOG.NAME,
-                name: DOG.BULL_DOG,
-                Description: DOG.FEMALE_PUPPY_BULL_DOG,
+                type: storeHelper.PRODUCTS.DOG.NAME,
+                name: storeHelper.PRODUCTS.DOG.BULL_DOG_ID,
+                Description: storeHelper.PRODUCTS.DOG.FEMALE_PUPPY_BULL_DOG_ID,
             },
         ], false);
         storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
-        storeHelper.verifyProductExistence([FISH.LARGE_ANGEL_FISH, CAT.TAILLESS_MAX, DOG.FEMALE_PUPPY_BULL_DOG])
+        storeHelper.removeFromCart([storeHelper.PRODUCTS.FISH.SMALL_ANGEL_FISH_ID, storeHelper.PRODUCTS.DOG.FEMALE_PUPPY_BULL_DOG_ID]);
+        storeHelper.verifyProductExistence([{val:storeHelper.PRODUCTS.FISH.SMALL_ANGEL_FISH_ID,isExist:false},{val:storeHelper.PRODUCTS.DOG.FEMALE_PUPPY_BULL_DOG_ID,isExist:false}])
     });
-    it.only("remove product from cart and make sure it is removed", () => {// bug in (EST-1)
-        storeHelper.addProducts([
-            {
-            type: FISH.NAME,
-            name: FISH.ANGEL_FISH,
-            Description: FISH.SMALL_ANGEL_FISH,
-        },
-        {
-            type: CAT.NAME,
-            name: CAT.MANX,
-            Description: CAT.TAILLESS_MAX,
-        },
-        {
-            type: DOG.NAME,
-            name: DOG.BULL_DOG,
-            Description: DOG.FEMALE_PUPPY_BULL_DOG,
-        },
-    ], false);
-        storeHelper.clickButton(storeHelper.LOCATORS.cart,false)
-        storeHelper.removeFromCart([FISH.SMALL_ANGEL_FISH, DOG.FEMALE_PUPPY_BULL_DOG]);
-        storeHelper.verifyProductExistence([FISH.SMALL_ANGEL_FISH, DOG.FEMALE_PUPPY_BULL_DOG],false)
+    it.only('verify "Retuen to main menu" button will redirect user to main page', () => {
+        storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
+        storeHelper.clickButton(storeHelper.WORD_REGISTRY.RETURN_TO_MAIN_PAGE_BUTTON)
+        storeHelper.testbuttonRedirect(storeHelper.WORD_REGISTRY.RETURN_TO_MAIN_PAGE_BUTTON);
     });
-    it('Verify click in "Return to main menu" button and redirect to main page', () => {
-
-        // cart->main menu
-        
-        // fish->type->click
-        // click in fish ->clich in type
-
-        storeHelper.checkButtonFunctionality(
-            storeHelper.WORD_REGISTRY.RETURN_TO_MAIN_PAGE_BUTTON
-        );
+    it.only(`verify"Return to 'product name'" button will redirect user to product page`, () => {
+        storeHelper.clickButton(storeHelper.LOCATORS.reptiles, false);
+        storeHelper.clickButton(storeHelper.PRODUCTS.REPTILES.IGUANA_ID);
+        storeHelper.clickButton(`Return to ${storeHelper.PRODUCTS.REPTILES.NAME}`)
+        storeHelper.testbuttonRedirect(storeHelper.PRODUCTS.REPTILES.NAME);
     });
-    it(`Verify click in "Return to 'product name'" button and return to product page`, () => {
-        storeHelper.checkButtonFunctionality1(storeHelper.LOCATORS.reptiles, REPTILES.IGUANA, "REPTILES");
-    });
-    it("verify from cart is empty before add any iteam", () => {
-        
-        storeHelper.clickButton(storeHelper.LOCATORS.cart, false);
-        storeHelper.textExistence(
-            storeHelper.WORD_REGISTRY.PRODUCTS_TABLE,
-            storeHelper.MESSAGE.EMPTY_MESSAGE
-        );
-    });
-    it('verify the cart will be empty after delete all product', () => {
-        storeHelper.addProducts([{
-                type: storeHelper.LOCATORS.fish,
-                name: FISH.ANGEL_FISH,
-                Description: FISH.LARGE_ANGEL_FISH,
-            },
-            {
-                type: storeHelper.LOCATORS.dog,
-                name: DOG.BULL_DOG,
-                Description: DOG.FEMALE_PUPPY_BULL_DOG,
-            },
-            {
-                type: storeHelper.LOCATORS.cat,
-                name: CAT.MANX,
-                Description: CAT.TAILLESS_MAX,
-            },
-        ]);
-        storeHelper.removeFromCart([FISH.LARGE_ANGEL_FISH, DOG.FEMALE_PUPPY_BULL_DOG, CAT.TAILLESS_MAX]);
-        storeHelper.textExistence(
-            storeHelper.WORD_REGISTRY.PRODUCTS_TABLE,
-            storeHelper.MESSAGE.EMPTY_MESSAGE
-        );
-
-    })
-    it("verify from total cost", () => {
+    it("check from total cost", () => {
         storeHelper.addProducts(
             [{
-                    type: storeHelper.LOCATORS.fish,
-                    name: FISH.ANGEL_FISH,
-                    Description: FISH.LARGE_ANGEL_FISH,
-                },
-                {
-                    type: storeHelper.LOCATORS.fish,
-                    name: FISH.ANGEL_FISH,
-                    Description: FISH.LARGE_ANGEL_FISH,
-                },
+                type: storeHelper.PRODUCTS.FISH.NAME,
+                name: storeHelper.PRODUCTS.FISH.ANGEL_FISH_ID,
+                Description: storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,
+            },
+            {
+                type: storeHelper.PRODUCTS.FISH.NAME,
+                name: storeHelper.PRODUCTS.FISH.ANGEL_FISH_ID,
+                Description: storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,
+            },
             ],
             false
         );
         storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
-        storeHelper.checkTotalCost(FISH.LARGE_ANGEL_FISH, 1)
+        storeHelper.checkTotalCost(storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID)
     });
-    it('verify from sub total', () => {
+    it('check from sub total', () => {
         storeHelper.addProducts([{
-                type: storeHelper.LOCATORS.fish,
-                name: FISH.ANGEL_FISH,
-                Description: FISH.LARGE_ANGEL_FISH,
-            },
-            {
-                type: storeHelper.LOCATORS.dog,
-                name: DOG.BULL_DOG,
-                Description: DOG.FEMALE_PUPPY_BULL_DOG,
-            },
-            {
-                type: storeHelper.LOCATORS.cat,
-                name: CAT.MANX,
-                Description: CAT.TAILLESS_MAX,
-            },
-        ]);
-
+            type: storeHelper.PRODUCTS.FISH.NAME,
+            name: storeHelper.PRODUCTS.FISH.ANGEL_FISH_ID,
+            Description: storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,
+        },
+        {
+            type: storeHelper.PRODUCTS.CAT.NAME,
+            name: storeHelper.PRODUCTS.CAT.MANX_ID,
+            Description: storeHelper.PRODUCTS.CAT.TAILLESS_MAX_ID,
+        },
+        {
+            type: storeHelper.PRODUCTS.DOG.NAME,
+            name: storeHelper.PRODUCTS.DOG.BULL_DOG_ID,
+            Description: storeHelper.PRODUCTS.DOG.FEMALE_PUPPY_BULL_DOG_ID,
+        },
+        ], false);
+        storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
         storeHelper.checkSubTotal()
     })
-    it('edit quantity and make sure it is modified', () => {
-        storeHelper.addProducts(
-            [{
-                type: storeHelper.LOCATORS.fish,
-                name: FISH.ANGEL_FISH,
-                Description: FISH.LARGE_ANGEL_FISH,
-            }, ],
-            false
-        )
-        storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
-        storeHelper.changeQantity(FISH.LARGE_ANGEL_FISH, '10')
-        storeHelper.verify('10', FISH.LARGE_ANGEL_FISH, true)
-    })
-    it('verify from change cost after change quantity', () => {
+    it('edit quantity and verify it is modified', () => {
         storeHelper.addProducts([{
-            type: storeHelper.LOCATORS.fish,
-            name: FISH.ANGEL_FISH,
-            Description: FISH.LARGE_ANGEL_FISH,
-        }, ]);
+            type: storeHelper.PRODUCTS.FISH.NAME,
+            name: storeHelper.PRODUCTS.FISH.ANGEL_FISH_ID,
+            Description: storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,
+        }
+        ]);
         storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
-        storeHelper.changeQantity(FISH.LARGE_ANGEL_FISH, '10')
-        storeHelper.checkTotalCost(FISH.LARGE_ANGEL_FISH, 1)
-
-
+        storeHelper.changeQantity(storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID, value)
+        storeHelper.verifyData(value, storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID)
     })
-    it('verify from product information', () => {
-        storeHelper.clickButton(storeHelper.LOCATORS.fish, false)
-        storeHelper.clickButton(FISH.ANGEL_FISH)
-        storeHelper.clickButton(FISH.LARGE_ANGEL_FISH)
-        storeHelper.verifyInformation(FISH.LARGE_ANGEL_FISH, FISH.DESCRIPTION, FISH.TYPE, FISH.PRICE)
+    it("verify that cart is empty before add any iteam", () => {
+        storeHelper.clickButton(storeHelper.LOCATORS.cart, false);
+        storeHelper.textExistence(storeHelper.LOCATORS.PRODUCTS_TABLE, storeHelper.MESSAGE.EMPTY_MESSAGE);
+    });
+    it('verify that cost change when change quantity', () => {
+        storeHelper.addProducts([{
+            type: storeHelper.PRODUCTS.FISH.NAME,
+            name: storeHelper.PRODUCTS.FISH.ANGEL_FISH_ID,
+            Description: storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,
+        }
+        ]);
+        storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
+        storeHelper.changeQantity(storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID, value)
+        storeHelper.checkTotalCost(storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID)
+    })
+    it('verify that cart table is empty after delete all product', () => {
+        storeHelper.addProducts([{
+            type: storeHelper.PRODUCTS.FISH.NAME,
+            name: storeHelper.PRODUCTS.FISH.ANGEL_FISH_ID,
+            Description: storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID,
+        },
+        {
+            type: storeHelper.PRODUCTS.CAT.NAME,
+            name: storeHelper.PRODUCTS.CAT.MANX_ID,
+            Description: storeHelper.PRODUCTS.CAT.TAILLESS_MAX_ID,
+        },
+        ]);
+        storeHelper.clickButton(storeHelper.LOCATORS.cart, false)
+        storeHelper.removeFromCart([storeHelper.PRODUCTS.FISH.LARGE_ANGEL_FISH_ID, storeHelper.PRODUCTS.CAT.TAILLESS_MAX_ID]);
+        storeHelper.textExistence(storeHelper.LOCATORS.PRODUCTS_TABLE, storeHelper.MESSAGE.EMPTY_MESSAGE);
+
     })
 });
--- cypress/support/JPetStoreHelper.js
+++ cypress/support/JPetStoreHelper.js
@@ -11,6 +11,9 @@ export const LOCATORS = {
   bird: '[src="../images/sm_birds.gif"]',
   cart: '[src="../images/cart.gif"]',
   tableRows: "table tbody tr",
+  PRODUCTS_TABLE: "table",
+  mainInMainPage : "#Main",
+  headerTwoInProductPage : "h2"
 };
 export const MESSAGE = {
   INVALID_CREDENTIALS_MESSAGE: "Invalid username or password.  Signon failed.",
@@ -20,9 +23,45 @@ export const WORD_REGISTRY = {
   SIGNIN: "Sign In",
   LOGIN: "Login",
   ADD_TO_CART_BUTTON: "Add to Cart",
-  PRODUCTS_TABLE: "table",
   RETURN_TO_MAIN_PAGE_BUTTON: "Return to Main Menu",
 };
+export const PRODUCTS = {
+  FISH : {
+    NAME: 'FISH',
+    NAME2: 'Fish',
+    ANGEL_FISH_ID: "FI-SW-01",
+    LARGE_ANGEL_FISH_ID: "EST-1",
+    SMALL_ANGEL_FISH_ID: "EST-2",
+    TYPE: 'Angelfish',
+    DESCRIPTION: 'Large Angelfish',
+    PRICE: '$16.50'
+  },
+  DOG : {
+    NAME: 'DOG',
+    NAME2: 'Dog',
+    BULL_DOG_ID: "K9-BD-01",
+    MALE_ADULT_BULL_DOG_ID: "EST-6",
+    FEMALE_PUPPY_BULL_DOG_ID: "EST-7",
+  },
+  CAT : {
+    NAME: 'CAT',
+    NAME2: 'Cat',
+    MANX_ID: "FL-DSH-01",
+    TAILLESS_MAX_ID: "EST-14",
+  },
+  REPTILES : {
+    NAME: 'REPTILES',
+    NAME2: 'Reptiles',
+    IGUANA_ID: "RP-LI-02",
+    GREEN_ADULT_IGUANA_ID: "EST-13",
+  },
+  BIRD : {
+    NAME: 'BIRD',
+    NAME2: 'Bird',
+    FINCH_ID: "AV-SB-02",
+    ADULT_MALE_FINCH_ID: "EST-19",
+  }
+}
 export const enterFieldValue = (value, locator) => {
   cy.get(locator).clear().invoke("val", value);
 };
@@ -33,6 +72,11 @@ export const clickButton = (locator, contain = true) => {
     cy.get(locator).click({ force: true });
   }
 };
+export const clickButtons = (buttons) => {
+  buttons.forEach((button) => {
+    clickButton(button.name, button.type)
+  })
+}
 export const signin = (username, password, validCred = true) => {
   enterFieldValue(username, LOCATORS.usernameField);
   enterFieldValue(password, LOCATORS.passwordField);
@@ -82,107 +126,40 @@ export const addProducts = (products, fromMenu = true) => {
     }
     clickButton(WORD_REGISTRY.RETURN_TO_MAIN_PAGE_BUTTON);
   });
-
-  // products.forEach((element) => {
-  //     clickButton(element.type, false);
-  //     clickButton(element.name);
-  //     if (fromMenu === true) {
-  //         cy.contains(element.Description)
-  //             .parent()
-  //             .parent()
-  //             .find(".Button")
-  //             .click();
-  //         cy.get("table").should("contain", element.Description);
-  //     } else {
-  //         clickButton(element.Description);
-  //         clickButton(WORD_REGISTRY.ADD_TO_CART_BUTTON);
-  //         // cy.get(WORD_REGISTRY.PRODUCTS_TABLE).should(
-  //         //     "contain",
-  //         //     element.Description
-  //         // );
-  //     }
-  //     clickButton(WORD_REGISTRY.RETURN_TO_MAIN_PAGE_BUTTON);
-  // });
 };
 export const verifyProductExistence = (product, isExist = true) => {
 
-    product.forEach(value=>{
-        if(isExist){
-            cy.get(WORD_REGISTRY.PRODUCTS_TABLE).should("contain", value);
-        }else{
-            cy.get(WORD_REGISTRY.PRODUCTS_TABLE).should('not.contain', value);
-        }
-    })
-//   if (isExist) {
-//     product.forEach((value) => {
-//       cy.get(WORD_REGISTRY.PRODUCTS_TABLE).should("contain", value);
-//     });
-//   } else {
-//     product.forEach((value) => {
-//       cy.get(LOCATORS.tableRows).should("not.contain", value);
-//     });
-//   }
+  product.forEach(value => {
+    if (value.isExist) {
+      cy.get(LOCATORS.PRODUCTS_TABLE).should("contain", value.val);
+    } else {
+      cy.get(LOCATORS.PRODUCTS_TABLE).should('not.contain', value.val);
+    }
+  })
 };
 export const removeFromCart = (products) => {
-  // clickButton(LOCATORS.cart, false)
   products.forEach((element) => {
     cy.contains(element).parent().parent().find(".Button").click();
-    // cy.get(LOCATORS.tableRows).should("not.contain", element + " ");
   });
-};
-export const checkButtonFunctionality = (button, page = "main") => {
-  //need edit
-  if (button == "Return to Main Menu") {
-    clickButton(LOCATORS.cart, false);
-    clickButton(button);
-    cy.get("#Main").should("exist");
-  } else if (button == "FISH") {
-    clickButton(LOCATORS.fish, false);
-    clickButton("FI-SW-01");
-    clickButton(`Return to ${button}`);
-    cy.get("h2").should("contain", "Fish");
-  } else if (button == "DOGS") {
-    clickButton(LOCATORS.dog, false);
-    clickButton("K9-BD-01");
-    clickButton(`Return to ${button}`);
-    cy.get("h2").should("contain", "Dogs");
-  } else if (button == "CATS") {
-    clickButton(LOCATORS.birds, false);
-    clickButton("FL-DSH-01");
-    clickButton(`Return to ${button}`);
-    cy.get("h2").should("contain", "Cats");
-  } else if (button == "REPTILES") {
-    clickButton(LOCATORS.reptiles, false);
-    clickButton("RP-LI-02");
-    clickButton(`Return to ${button}`);
-    cy.get("h2").should("contain", "Reptiles");
-  } else if (button == "BIRDS") {
-    clickButton(LOCATORS.bird, false);
-    clickButton("AV-CB-01");
-    clickButton(`Return to ${button}`);
-    cy.get("h2").should("contain", "Birds");
-  }
 };
 export const textExistence = (locator, text) => {
   cy.get(locator).should("contain", text);
 };
-export const checkTotalCost = (locator, row) => {
-  cy.get(`input[name="${locator}"]`).then(($input) => {
+export const checkTotalCost = (locator, row = 2) => {
+  cy.get(`input[name="${locator}"]`).then((input) => {
     //quantity
-    const value = parseFloat($input.val());
-    cy.get(LOCATORS.tableRows)
-      .eq(row)
-      .children()
-      .eq(5)
+    const value = parseFloat(input.val());
+    cy.get(LOCATORS.PRODUCTS_TABLE)
+      .find(`tr:nth-child(${row})`)
+      .find('td:nth-child(6)')
       .invoke("text")
       .then((text) => {
         //price
         const price = parseFloat(text.replace(/\$/g, ""));
         const total = (value * price).toFixed(2);
-        cy.get(LOCATORS.tableRows)
-          .eq(row)
-          .children()
-          .eq(6)
+        cy.get(LOCATORS.PRODUCTS_TABLE)
+          .find(`tr:nth-child(${row})`)
+          .find('td:nth-child(7)')
           .invoke("text")
           .should("eq", "$" + total);
       });
@@ -191,33 +168,36 @@ export const checkTotalCost = (locator, row) => {
 export const checkSubTotal = () => {
   clickButton(LOCATORS.cart, false);
   cy.get(LOCATORS.tableRows).then((value) => {
-    var sum = 0;
+    let sum = 0;
+    let sum2 = 0;
     value.slice(1, -1).each((index, row) => {
       cy.wrap(row).then((x) => {
         cy.get(row)
-          .children()
-          .eq(5)
+          .find('td:nth-child(7)')
           .invoke("text")
           .then((text) => {
             const value = parseFloat(text.replace(/\$/g, ""));
             if (!isNaN(value)) {
+              sum2++
               sum += value;
+              if (sum2 == 3) {
+                cy.contains('Sub Total:').invoke('text').then((text) => {
+                  const subtotal = text.match(/\$([\d.]+)/)[1];
+                  cy.log(subtotal);
+                  cy.wrap(subtotal).should('eq', sum + '0')
+                });
+              }
+              // cy.log(sum)
             }
           });
-      });
-    });
+      })
+    })
   });
-  /**
-     * cy.contains('Sub Total:').invoke('text').then((text) => { to give sub total
-        const subtotal = text.match(/\$([\d.]+)/)[1];
-        cy.log(subtotal);
-    });
-     */
 };
 export const changeQantity = (locator, number) => {
   cy.get(`input[name="${locator}"]`).clear().type(`${number}{enter}`);
 };
-export const verify = (data, row, quantity = false) => {
+export const verifyData = (data, row, quantity = true) => {
   if (quantity == false) {
     cy.get("tr")
       .eq(row)
@@ -229,52 +209,27 @@ export const verify = (data, row, quantity = false) => {
   } else {
     cy.get(`input[name="${row}"]`).invoke("val").should("eq", data);
   }
+
+  // cy.get(`input[name="${locator}"]`).invoke("val").should("eq", data);
 };
 export const verifyInformation = (ID, description, type, price) => {
-  verify(ID, 1);
-  verify(description, 2);
-  verify(type, 3);
-  verify(price, 5);
+  verifyData(ID, 1, false);
+  verifyData(description, 2, false);
+  verifyData(type, 3, false);
+  verifyData(price, 5, false);
 };
-export const checkButtonFunctionality1 = (
-  locator,
-  type,
-  button,
-  page = "main"
-) => {
-  if (button == "Return to Main Menu") {
-    clickButton(LOCATORS.cart, false);
-    clickButton(button);
-    cy.get("#Main").should("exist");
-  } else {
-    clickButton(locator, false);
-    clickButton(type);
-    clickButton(`Return to ${button}`);
-    if (button == "FISH") {
-      cy.get("h2").should("contain", "Fish");
-    } else if (button == "DOGS") {
-      cy.get("h2").should("contain", "Dogs");
-    } else if (button == "CATS") {
-      cy.get("h2").should("contain", "Cats");
-    } else if (button == "REPTILES") {
-      cy.get("h2").should("contain", "Reptiles");
-    } else if (button == "BIRDS") {
-      cy.get("h2").should("contain", "Birds");
-    }
+export const testbuttonRedirect = (button) => {
+  if (button == WORD_REGISTRY.RETURN_TO_MAIN_PAGE_BUTTON) {
+    cy.get(LOCATORS.mainInMainPage).should("exist");
+  } else if(button == PRODUCTS.FISH.NAME){
+    cy.get(LOCATORS.headerTwoInProductPage).should("contain", PRODUCTS.FISH.NAME2);
+  } else if (button == PRODUCTS.DOG.NAME) {
+    cy.get(LOCATORS.headerTwoInProductPage).should("contain", PRODUCTS.DOG.NAME2);
+  } else if (button == PRODUCTS.CAT.NAME) {
+    cy.get(LOCATORS.headerTwoInProductPage).should("contain", PRODUCTS.CAT.NAME2);
+  } else if (button == PRODUCTS.REPTILES.NAME) {
+    cy.get(LOCATORS.headerTwoInProductPage).should("contain", PRODUCTS.REPTILES.NAME2);
+  } else if (button == PRODUCTS.BIRD.NAME) {
+    cy.get(LOCATORS.headerTwoInProductPage).should("contain", PRODUCTS.BIRD.NAME2);
   }
 };
-// export const givePrice = (row) => {
-//     cy.get("table tbody tr")
-//         .eq(row)
-//         .children()
-//         .eq(5)
-//         .invoke('text')
-//         .then((value) => {
-//             // cy.log(value0)
-//             const stringWithDollar = value;
-//             const stringWithoutDollar = stringWithDollar.replace(/\$/g, "");
-//             //   cy.log(stringWithoutDollar)
-//             return stringWithoutDollar;
-//         })
-
-// }
